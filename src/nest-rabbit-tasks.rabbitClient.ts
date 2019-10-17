700da06e2b099d3434761f16b7002beb3bb2b2e3
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-10-17 15:36:48 +0200

    :sparkles: add immutableInfrastructure

--- src/nest-rabbit-tasks.rabbitClient.ts
+++ src/nest-rabbit-tasks.rabbitClient.ts
@@ -1,15 +1,51 @@
-import { NestRabbitTasksQueueOptions } from './nest-rabbit-tasks.interfaces';
+import { Logger } from '@nestjs/common';
 import { Haredo, Queue, HaredoChain } from 'haredo';
 
+import { NestRabbitTasksQueueOptions } from './nest-rabbit-tasks.interfaces';
+
+let debugMessageCounter = 0;
+
 export class NestRabbitTasksRabbitClient {
-  public static async buildQueueConnection(option: NestRabbitTasksQueueOptions): Promise<HaredoChain> {
-    // TODO: if imutableStructure, prevent queue creation
-    // check that queue exist or throw
+  public static async buildQueueConnection(option: NestRabbitTasksQueueOptions, logger: Logger): Promise<HaredoChain | null> {
+    const connection = new Haredo({ connection: option.amqpOptions.connectionUrl });
+
+    if (option.globalOptions.immutableInfrastructure) {
+      const channel = await connection.connectionManager.getChannel();
+      try {
+        await channel.checkQueue(option.entity.queueName);
+      } catch (err) {
+        if (err.code === 404) {
+          const msg = `You have activated "globalOptions.immutableInfrastructure".
+          This prevents nest-rabbit-tasks to create non-existent queue.
+          The queue ${option.entity.queueName} does not exist.
+          Please create it in RabbitMQ interface.`;
+          logger.error(msg);
+          return null;
+        }
+        throw err;
+      }
+    }
+
     const queue = new Queue(option.entity.queueName);
-    return new Haredo({ connection: option.amqpOptions.connectionUrl })
-      .queue(queue)
-      .autoAck(false)
-      .reestablish(true)
-      .prefetch(option.globalOptions.prefetchSize || 1);
+
+    return (
+      connection
+        .queue(queue)
+        // we do not want to auto acknowledge the messages
+        .autoAck(false)
+        .reestablish(true)
+        // we do want to set the content-type to JSON
+        .json(true)
+        .prefetch(option.globalOptions.prefetchSize || 1)
+        .use(async (_, next) => {
+          const counter = debugMessageCounter++;
+          const timeStart = process.hrtime();
+
+          logger.debug(`Start Handling message ${counter}`);
+          await next();
+          const processTime = process.hrtime(timeStart);
+          logger.debug(`Done Handling message ${counter} in ${processTime[0]}s and ${processTime[1] / 1000000}ms`);
+        })
+    );
   }
 }
--- src/nest-rabbit-worker.dynamic.ts
+++ src/nest-rabbit-worker.dynamic.ts
@@ -12,10 +12,12 @@ import { NestRabbitTasksExplorer } from './nest-rabbit-tasks.explorer';
 
 type Dynamics = Omit<DynamicModule, 'module'>;
 
+const logger = new Logger('AMQPRabbitTaskModule');
+
 export class NestRabbitWorkerDynamic {
   public static getSyncDynamics(options: NestRabbitTasksModuleSyncOptions | NestRabbitTasksModuleSyncOptions[]): Dynamics {
     const normalizedOptions = _.flatten([options]);
-    const rabbitQueueProviders = NestRabbitWorkerDynamic.createQueueConnectionProvider(normalizedOptions);
+    const rabbitQueueProviders = NestRabbitWorkerDynamic.createQueueConnectionProvider(normalizedOptions, logger);
     return {
       providers: [
         ...rabbitQueueProviders,
@@ -28,21 +30,21 @@ export class NestRabbitWorkerDynamic {
 
   public static getAsyncDynamics(options: NestRabbitTasksModuleAsyncOptions | NestRabbitTasksModuleAsyncOptions[]): Dynamics {
     const normalizedOptions = _.flatten([options]);
-    console.log(normalizedOptions);
+    console.log(normalizedOptions, logger);
     return { imports: [], providers: [], exports: [] };
   }
 
-  private static otherProviders: Provider[] = [
-    NestRabbitTasksExplorer,
-    { provide: Logger, useValue: new Logger('AMQPRabbitTaskModule') },
-  ];
+  private static otherProviders: Provider[] = [NestRabbitTasksExplorer, { provide: Logger, useValue: logger }];
 
-  private static createQueueConnectionProvider(normalizedOptions: NestRabbitTasksModuleSyncOptions[]): Provider[] {
+  private static createQueueConnectionProvider(
+    normalizedOptions: NestRabbitTasksModuleSyncOptions[],
+    logger: Logger
+  ): Provider[] {
     return _(normalizedOptions)
       .filter(option => option.entityType === 'queue')
       .map((filteredOption: NestRabbitTasksModuleSyncOptions) => ({
         provide: NestRabbitWorkerToken.getTokenForQueueConnection(filteredOption.reference),
-        useFactory: (opt: NestRabbitTasksQueueOptions) => NestRabbitTasksRabbitClient.buildQueueConnection(opt),
+        useFactory: (opt: NestRabbitTasksQueueOptions) => NestRabbitTasksRabbitClient.buildQueueConnection(opt, logger),
         inject: [NestRabbitWorkerToken.getTokenForQueueConnectionOptions(filteredOption.reference)],
       }))
       .value();
