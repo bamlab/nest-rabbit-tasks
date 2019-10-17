a7e6352b1bb1bb4b6ba07ecf7590ce9bae050bb7
MattAgn <matthieua@bam.tech>
2019-10-17 10:35:27 +0200

    :construction: wip

--- src/nest-rabbit-tasks.module.ts
+++ src/nest-rabbit-tasks.module.ts
@@ -1,21 +1,20 @@
-import { OnModuleInit, Module, DynamicModule } from '@nestjs/common';
-import { ModuleRef, ModulesContainer, Reflector } from '@nestjs/core';
-import { MetadataScanner } from '@nestjs/core/metadata-scanner';
-import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
+import { OnModuleInit, Module as ModuleDecorator, DynamicModule } from '@nestjs/common';
+import { ModuleRef, ModulesContainer } from '@nestjs/core';
+// import { MetadataScanner } from '@nestjs/core/metadata-scanner';
+// import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 
-import { HaredoChain, MessageCallback } from 'haredo';
+// import { HaredoChain, MessageCallback } from 'haredo';
 import _ from 'lodash';
 
-import {
-  NestRabbitTasksModuleSyncOptions,
-  NestRabbitTasksModuleAsyncOptions,
-  RabbitWorkerInterface,
-} from './nest-rabbit-tasks.interfaces';
+import { NestRabbitTasksModuleSyncOptions, NestRabbitTasksModuleAsyncOptions } from './nest-rabbit-tasks.interfaces';
 import { NestRabbitWorkerDynamic } from './nest-rabbit-worker.dynamic';
-import { NEST_RABBIT_TASKS_WORKER, WorkerDecoratorOptions } from './nest-rabbit-tasks.decorator';
-import { NestRabbitWorkerToken } from './nest-rabbit-worker.token';
+import { Module } from '@nestjs/core/injector/module';
+import { Injectable, Provider } from '@nestjs/common/interfaces';
+import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
+// import { NEST_RABBIT_TASKS_WORKER, WorkerDecoratorOptions } from './nest-rabbit-tasks.decorator';
+// import { NestRabbitWorkerToken } from './nest-rabbit-worker.token';
 
-@Module({})
+@ModuleDecorator({})
 export class NestRabbitTasksModule implements OnModuleInit {
   public static registerSync(options: NestRabbitTasksModuleSyncOptions | NestRabbitTasksModuleSyncOptions[]): DynamicModule {
     return {
@@ -33,8 +32,7 @@ export class NestRabbitTasksModule implements OnModuleInit {
 
   public constructor(
     readonly modulesContainer: ModulesContainer,
-    readonly moduleRef: ModuleRef,
-    private readonly reflector: Reflector
+    readonly moduleRef: ModuleRef // private readonly reflector: Reflector
   ) {}
 
   public onModuleInit() {
@@ -42,35 +40,49 @@ export class NestRabbitTasksModule implements OnModuleInit {
   }
 
   private scanAndBindRabbitTasksWorkerToQueueConnection() {
-    const allInjectedModules = [...this.modulesContainer.values()];
+    const allInjectedModules: Module[] = [...this.modulesContainer.values()];
 
-    _(allInjectedModules)
-      .flatMap(module => [...module.components.values()])
-      .filter(
-        instanceWrapper => instanceWrapper.metatype && !!this.reflector.get(NEST_RABBIT_TASKS_WORKER, instanceWrapper.metatype)
-      )
-      .map(({ instance, metatype }: InstanceWrapper<RabbitWorkerInterface<any>>) => {
-        const metadata = this.reflector.get<WorkerDecoratorOptions>(NEST_RABBIT_TASKS_WORKER, metatype);
-        let queueConnection: HaredoChain;
-        try {
-          queueConnection = this.moduleRef.get<HaredoChain>(NestRabbitWorkerToken.getTokenForQueueConnection(metadata.reference));
-        } catch (err) {
-          // TODO: log error
-          // no queue found with name
-          throw err;
-        }
-        return { worker: instance, queueConnection };
-      })
-      .map(({ worker, queueConnection }: { worker: RabbitWorkerInterface<any>; queueConnection: HaredoChain }) => {
-        return new MetadataScanner().scanFromPrototype(worker, Object.getPrototypeOf(worker), name => {
-          if (name === 'handleMessage') {
-            queueConnection.subscribe(worker.handleMessage as MessageCallback<any>).catch(() => {
-              // TODO: log errors
-            });
-          }
-          Promise.resolve(true);
-        });
-      })
-      .value();
+    let providers: Provider[] = [];
+
+    for (let [_, container] of this.modulesContainer) {
+      for (let [_, module] of container.providers.values()) {
+        providers.push(module);
+      }
+    }
+    allInjectedModules.map(module => {
+      console.log(typeof module);
+    });
+
+    // _(allInjectedModules)
+    //   .flatMap(module => {
+    //     console.log(Object.keys(module));
+    //     return [...module.providers.values()];
+    //   })
+    //   .filter(
+    //     instanceWrapper => instanceWrapper.metatype && !!this.reflector.get(NEST_RABBIT_TASKS_WORKER, instanceWrapper.metatype)
+    //   )
+    //   .map(({ instance, metatype }: InstanceWrapper<RabbitWorkerInterface<any>>) => {
+    //     const metadata = this.reflector.get<WorkerDecoratorOptions>(NEST_RABBIT_TASKS_WORKER, metatype);
+    //     let queueConnection: HaredoChain;
+    //     try {
+    //       queueConnection = this.moduleRef.get<HaredoChain>(NestRabbitWorkerToken.getTokenForQueueConnection(metadata.reference));
+    //     } catch (err) {
+    //       // TODO: log error
+    //       // no queue found with name
+    //       throw err;
+    //     }
+    //     return { worker: instance, queueConnection };
+    //   })
+    //   .map(({ worker, queueConnection }: { worker: RabbitWorkerInterface<any>; queueConnection: HaredoChain }) => {
+    //     return new MetadataScanner().scanFromPrototype(worker, Object.getPrototypeOf(worker), name => {
+    //       if (name === 'handleMessage') {
+    //         queueConnection.subscribe(worker.handleMessage as MessageCallback<any>).catch(() => {
+    //           // TODO: log errors
+    //         });
+    //       }
+    //       Promise.resolve(true);
+    //     });
+    //   })
+    //   .value();
   }
 }
